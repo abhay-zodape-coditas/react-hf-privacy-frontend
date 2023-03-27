7390eb4b531f29cca17a5682400767b15007d2a9
Samkit Kothari <samkit.kothari@coditas.com>
2023-03-27 15:34:15 +0530

    scroll into view added

--- src/components/pageHeader/index.tsx
+++ src/components/pageHeader/index.tsx
@@ -1,13 +1,16 @@
 import { LanguageContext } from "../../hoc/languageProvider";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import BrandingLogo from "../../assets/images/Hydrafacial_Pos_Trademark.svg";
 import { Col, Container, Row } from "react-bootstrap";
 import styles from "./pageHeader.module.scss";
 
 const PageHeader = () => {
   const { localstring } = useContext(LanguageContext);
+  useEffect(() => {
+    document.getElementById("header")?.scrollIntoView({ behavior: "auto" });
+  }, []);
   return (
-    <Container fluid className={styles.containerWrapper}>
+    <Container fluid className={styles.containerWrapper} id="header">
       <Row className={styles.contentWrapper}>
         <Col>
           <img
